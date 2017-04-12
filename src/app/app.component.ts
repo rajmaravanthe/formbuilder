005842bf8575361ce99b899d0700b79794576fc6
rajesha maravanthe <rraajjpoojary@gmail.com>
2017-04-12 14:44:39 +0530

    added new files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { ListService } from "app/service/list.service";
+import { DropDownService } from "app/service/drop-down.service";
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ListService]
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string;
+  name: string;
+  lastName: string;
+  userDetail = {};
+  arrayList: any[] = [];
+  genderArray: any[] = [];
+  nameInChar: boolean = true;
+  lastNameInChar: boolean = true;
+
+  constructor(private _listService: ListService, private _dropDownService: DropDownService, public fb: FormBuilder) { }
+ 
+  registerForm = this.fb.group({
+    name: ['', Validators.required],
+    lastName: ['', Validators.required],
+    age: ['', Validators.required],
+    genderType: ['', Validators.required]
+  })
+
+  ngOnInit() {
+    this.userDetail = {
+      name: '',
+      lastName: '',
+      age: '',
+      genderType: ''
+    }
+    this.genderArray = this._dropDownService.getDropDown();
+  }
+
+checkChar(data) {
+    var regex = /^[a-zA-Z ]*$/;
+    if(data.name === "name") {
+      this.nameInChar = regex.test(data.value);
+    } else {
+      this.lastNameInChar = regex.test(data.value);
+    }
+  }
+  submit(values, isValid) {
+    if (!isValid) {
+      return;
+    }
+    console.log(values);
+    let model = {
+      name: values.name,
+      lastName: values.lastName,
+      age: values.age,
+      gender: values.gender
+    }
+    this._listService.addList(model);
+    this.arrayList = this._listService.getList();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DropDownService } from "app/service/drop-down.service";
 
 @NgModule({
   declarations: [
@@ -12,9 +13,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [DropDownService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
